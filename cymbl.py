063429308dbdff60d1c07d387a76caa43c58ceb3
William S. Moses <gh@wsmoses.com>
2023-02-05 01:17:07 -0500

    fixup julia and add conf

--- cymbl.py
+++ cymbl.py
@@ -2,6 +2,7 @@
 
 from flask import *
 import requests
+requests.packages.urllib3.util.connection.HAS_IPV6 = False
 
 app = Flask(__name__)
 import sys
@@ -13,7 +14,7 @@ def proxy(path):
     try:
         import sys
         #print(request.headers, file=sys.stderr)
-        resp = requests.get('http://wsmoses.github.io/'+path, headers=request.headers)
+        resp = requests.get('https://enzymead.github.io/'+path, headers=request.headers)
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
         headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
         #print(headers,file=sys.stderr)
@@ -23,18 +24,35 @@ def proxy(path):
         return str(e)
 
 @app.route('/julia', defaults={'path': ''})
+@app.route('/julia/', defaults={'path': ''})
 @app.route('/julia/<path:path>')
-def get_resource(path):  # pragma: no cover
-    mimetypes = {
-        ".css": "text/css",
-        ".html": "text/html",
-        ".js": "application/javascript",
-    }
-    complete_path = os.path.join(root_dir(), "julia", path)
-    ext = os.path.splitext(path)[1]
-    mimetype = mimetypes.get(ext, "text/html")
-    content = get_file(complete_path)
-    return Response(content, mimetype=mimetype)
+def jl_get_resource(path):  # pragma: no cover
+    try:
+        import sys
+        #print(request.headers, file=sys.stderr)
+        resp = requests.get('https://enzymead.github.io/Enzyme.jl/'+path, allow_redirects=False) # , headers=request.headers)
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+        #print(headers,file=sys.stderr)
+        res = Response(resp.content, resp.status_code, headers)
+        return res
+    except Exception as e:
+        return str(e)
+
+@app.route('/conference', defaults={'path': ''})
+@app.route('/conference/<path:path>')
+def conf_get_resource(path):  # pragma: no cover
+    try:
+        import sys
+        #print(request.headers, file=sys.stderr)
+        resp = requests.get('https://enzymead.github.io/enzyme-conf-website/'+path) # , headers=request.headers)
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+        #print(headers,file=sys.stderr)
+        res = Response(resp.content, resp.status_code, headers)
+        return res
+    except Exception as e:
+        return str(e)
 
 @app.route('/doxygen', defaults={'path': ''})
 @app.route('/doxygen/<path:path>')
