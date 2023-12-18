d6feee137021451e3cae760ada48596d11855a7e
William S. Moses <gh@wsmoses.com>
2023-12-18 18:23:25 -0500

    Update config for rust

--- cymbl.py
+++ cymbl.py
@@ -1,5 +1,3 @@
-#!/bin/python3
-
 from flask import *
 import requests
 requests.packages.urllib3.util.connection.HAS_IPV6 = False
@@ -39,6 +37,22 @@ def jl_get_resource(path):  # pragma: no cover
     except Exception as e:
         return str(e)
 
+@app.route('/rust', defaults={'path': ''})
+@app.route('/rust/', defaults={'path': ''})
+@app.route('/rust/<path:path>')
+def rs_get_resource(path):  # pragma: no cover
+    try:
+        import sys
+        #print(request.headers, file=sys.stderr)
+        resp = requests.get('https://enzymead.github.io/rustbook/'+path, allow_redirects=False) # , headers=request.headers)
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+        #print(headers,file=sys.stderr)
+        res = Response(resp.content, resp.status_code, headers)
+        return res
+    except Exception as e:
+        return str(e)
+
 @app.route('/conference', defaults={'path': ''})
 @app.route('/conference/<path:path>')
 def conf_get_resource(path):  # pragma: no cover
@@ -63,20 +77,6 @@ def dox_get_resource(path):  # pragma: no cover
         ".js": "application/javascript",
     }
     complete_path = os.path.join(root_dir(), "doxygen", path)
-    ext = os.path.splitext(path)[1]
-    mimetype = mimetypes.get(ext, "text/html")
-    content = get_file(complete_path)
-    return Response(content, mimetype=mimetype)
-
-@app.route('/rust', defaults={'path': ''})
-@app.route('/rust/<path:path>')
-def rust_get_resource(path):  # pragma: no cover
-    mimetypes = {
-        ".css": "text/css",
-        ".html": "text/html",
-        ".js": "application/javascript",
-    }
-    complete_path = os.path.join(root_dir(), "rust", path)
     ext = os.path.splitext(path)[1]
     mimetype = mimetypes.get(ext, "text/html")
     content = get_file(complete_path)
