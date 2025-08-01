4c47085e6edc61c2878b85d0914330ba2db10303
William S. Moses <gh@wsmoses.com>
2025-08-01 12:21:36 -0400

    fix routinng

--- cymbl.py
+++ cymbl.py
@@ -54,6 +54,7 @@ def rs_get_resource(path):  # pragma: no cover
         return str(e)
 
 @app.route('/conference', defaults={'path': ''})
+@app.route('/conference/', defaults={'path': ''})
 @app.route('/conference/<path:path>')
 def conf_get_resource(path):  # pragma: no cover
     try:
@@ -69,18 +70,20 @@ def conf_get_resource(path):  # pragma: no cover
         return str(e)
 
 @app.route('/doxygen', defaults={'path': ''})
+@app.route('/doxygen/', defaults={'path': ''})
 @app.route('/doxygen/<path:path>')
 def dox_get_resource(path):  # pragma: no cover
-    mimetypes = {
-        ".css": "text/css",
-        ".html": "text/html",
-        ".js": "application/javascript",
-    }
-    complete_path = os.path.join(root_dir(), "doxygen", path)
-    ext = os.path.splitext(path)[1]
-    mimetype = mimetypes.get(ext, "text/html")
-    content = get_file(complete_path)
-    return Response(content, mimetype=mimetype)
+    try:
+        import sys
+        # print(request.headers, file=sys.stderr)
+        resp = requests.get('https://enzymead.github.io/Enzyme/'+path, allow_redirects=False) # , headers=request.headers)
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+        # print(headers,file=sys.stderr)
+        res = Response(resp.content, resp.status_code, headers)
+        return res
+    except Exception as e:
+        return str(e)
 
 @app.route('/')
 def root():
